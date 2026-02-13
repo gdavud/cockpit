eb38babb1edc56fb27873427944b595def93a853
davud600 <davudg2002@gmail.com>
2026-02-13 11:04:13 +0100

    added share files

--- .config/nvim/lua/davud/lazy/lsp.lua
+++ .config/nvim/lua/davud/lazy/lsp.lua
@@ -1,22 +1,4 @@
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "lua",
-	callback = function()
-		vim.bo.tabstop = 2
-		vim.bo.shiftwidth = 2
-		vim.bo.expandtab = true
-	end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
-	callback = function()
-		vim.bo.tabstop = 2
-		vim.bo.shiftwidth = 2
-		vim.bo.expandtab = true
-	end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "cpp", "h", "c" },
 	callback = function()
 		vim.bo.tabstop = 4
@@ -234,69 +216,7 @@ return {
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				clangd = {},
-
-				-- gopls = {},
-
-				pyright = {},
-
-				-- rust_analyzer = {},
-				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-				--
-				-- Some languages (like typescript) have entire language plugins that can be useful:
-				--    https://github.com/pmizio/typescript-tools.nvim
-				--
-				-- But for many setups, the LSP (`ts_ls`) will work just fine
-				ts_ls = {},
-
-				lua_ls = {
-					-- cmd = { ... },
-					-- filetypes = { ... },
-					-- capabilities = {},
-					settings = {
-						Lua = {
-							completion = {
-								callSnippet = "Replace",
-							},
-							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-							-- diagnostics = { disable = { 'missing-fields' } },
-						},
-					},
-				},
-
-				postgres_lsp = {},
 			}
-
-			-- To check the current status of installed tools and/or manually install
-			-- other tools, you can run
-			--    :Mason
-			--
-			-- You can press `g?` for help in this menu.
-			--
-			-- `mason` had to be setup earlier: to configure its options see the
-			-- `dependencies` table for `nvim-lspconfig` above.
-			--
-			-- You can add other tools here that you want Mason to install
-			-- for you, so that they are available from within Neovim.
-			local ensure_installed = vim.tbl_keys(servers or {})
-			vim.list_extend(ensure_installed, {
-				"stylua", -- Used to format Lua code
-			})
-			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-			require("mason-lspconfig").setup({
-				ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-				automatic_installation = false,
-				handlers = {
-					function(server_name)
-						local server = servers[server_name] or {}
-						-- This handles overriding only values explicitly passed
-						-- by the server configuration above. Useful when disabling
-						-- certain features of an LSP (for example, turning off formatting for ts_ls)
-						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-						require("lspconfig")[server_name].setup(server)
-					end,
-				},
-			})
 		end,
 	},
 
@@ -331,22 +251,12 @@ return {
 				end
 			end,
 			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "black", "isort" },
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				markdown = { "prettier" },
-				html = { "prettier" },
-				css = { "prettier" },
-				scss = { "prettier" },
 				c = { "clang_format" },
+				cc = { "clang_format" },
 				cpp = { "clang_format" },
 				h = { "clang_format" },
-				sql = { "pg_format" },
+				hh = { "clang_format" },
+				hpp = { "clang_format" },
 			},
 		},
 	},
@@ -357,32 +267,32 @@ return {
 		version = "1.*",
 		dependencies = {
 			-- Snippet Engine
-			{
-				"L3MON4D3/LuaSnip",
-				version = "2.*",
-				build = (function()
-					-- Build Step is needed for regex support in snippets.
-					-- This step is not supported in many windows environments.
-					-- Remove the below condition to re-enable on windows.
-					if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-						return
-					end
-					return "make install_jsregexp"
-				end)(),
-				dependencies = {
-					-- `friendly-snippets` contains a variety of premade snippets.
-					--    See the README about individual language/framework/plugin snippets:
-					--    https://github.com/rafamadriz/friendly-snippets
-					-- {
-					--   'rafamadriz/friendly-snippets',
-					--   config = function()
-					--     require('luasnip.loaders.from_vscode').lazy_load()
-					--   end,
-					-- },
-				},
-				opts = {},
-			},
-			"folke/lazydev.nvim",
+			-- {
+			-- 	"L3MON4D3/LuaSnip",
+			-- 	version = "2.*",
+			-- 	build = (function()
+			-- 		-- Build Step is needed for regex support in snippets.
+			-- 		-- This step is not supported in many windows environments.
+			-- 		-- Remove the below condition to re-enable on windows.
+			-- 		if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+			-- 			return
+			-- 		end
+			-- 		return "make install_jsregexp"
+			-- 	end)(),
+			-- 	dependencies = {
+			-- 		-- `friendly-snippets` contains a variety of premade snippets.
+			-- 		--    See the README about individual language/framework/plugin snippets:
+			-- 		--    https://github.com/rafamadriz/friendly-snippets
+			-- 		-- {
+			-- 		--   'rafamadriz/friendly-snippets',
+			-- 		--   config = function()
+			-- 		--     require('luasnip.loaders.from_vscode').lazy_load()
+			-- 		--   end,
+			-- 		-- },
+			-- 	},
+			-- 	opts = {},
+			-- },
+			-- "folke/lazydev.nvim",
 		},
 		--- @module 'blink.cmp'
 		--- @type blink.cmp.Config
@@ -434,7 +344,7 @@ return {
 				},
 			},
 
-			snippets = { preset = "luasnip" },
+			-- snippets = { preset = "luasnip" },
 
 			-- Blink.cmp includes an optional, recommended rust fuzzy matcher,
 			-- which automatically downloads a prebuilt binary when enabled.
@@ -443,7 +353,7 @@ return {
 			-- the rust implementation via `'prefer_rust_with_warning'`
 			--
 			-- See :h blink-cmp-config-fuzzy for more information
-			fuzzy = { implementation = "lua" },
+			-- fuzzy = { implementation = "lua" },
 
 			-- Shows a signature help window while you type arguments for a function
 			signature = { enabled = true },
